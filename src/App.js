bc3b9f546507cd6b587fb4500442b99b4ee5d484
RubenRock <ramses673@hotmail.com>
2021-02-07 10:24:10 -0600

    configurando crud firestore

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
+import {useState} from 'react'
 import logo from './logo.svg';
+import logo01 from './img/logo.png'
 import './App.css';
 import {Button} from '@material-ui/core';
 import { AccessAlarm, ThreeDRotation, Menu } from '@material-ui/icons';
-import NavBar from './components/demo'
+import NavBar from './components/navbar/demo'
+import * as Conexion from './components/firebase/conexionFirestore'
 
 
 function App() {
+  const [productos, setProductos] = useState([])
+  const [carga, setCarga] = useState(false)
+
+  const leerProductos = async() => {
+    console.log('pushado')
+    let resul = await Conexion.descargarNube()
+    console.log(resul)
+    setProductos(resul)
+    setCarga(true)
+  }
+
   return (
     <div className="App">      
       <NavBar  />
+
+      {/* PORTADA */}
+      <div className="portada" >
+          <img src={logo01} alt="logo01" style={{marginTop:20}}/>           
+          <p>Publicidad - Ventas - Rifas</p>  
+      </div>
+
+
+      {/* PRODUCTOS */}
+      {/* {Conexion.subirNube([{ titulo : 'Hola desde el titulo', descripcion : 'descripcion', clave : 'clave', fecha : 'fecha'}])}  */}
       <header className="App-header">  
-        <Button variant="contained" color="primary" onClick={()=> console.log('Hola meco')}>
+        <b className="productos_titulo">PRODUCTOS</b>
+
+        
+
+        <Button variant="contained" color="primary" onClick={()=> leerProductos()}>
           Hola Mundo!
         </Button>
         <AccessAlarm/>
---
+++ src/components/firebase/conexionFirestore.js
@@ -0,0 +1,62 @@
+import DbFirestore from './configFirestore'
+
+ /* const data ={
+    titulo:'orale',
+    uno:'primero',
+    dos:'segundo',
+    estado:'false'
+}  */
+
+//DbFirestore.collection('ejemplo').add(data)
+//DbFirestore.collection('ejemplo').doc(data.titulo).set(data)
+
+//DbFirestore.collection('ejemplo').get()
+//.then(item=> item.forEach(x => console.log(x.data())))
+
+
+//DbFirestore.collection('ejemplo').doc('z9zmivG81h8Nk832IViD').delete().then(()=> console.log('hecho'))
+
+export const descargarNube = () => new Promise((resolve, reject) =>{
+    let resul= []
+
+    DbFirestore.collection('boveda').get()
+    .then(item=> item.forEach(x => 
+        resul.push(x.data())        
+    ))  //hasta que termine de leer todos los datos ejecutamos el resolve
+    .then(() => resolve(resul))    
+})
+
+export const subirNube = (titulos) =>{        
+    console.log(titulos)
+    let data =[...titulos]    
+    let error=''
+    data.forEach(item => {                //nombre documento  //datos del docuemnto
+        DbFirestore.collection('boveda').doc(item.titulo).set(item).catch(e => error=e)
+    })
+    if (!error)
+       alert('Proceso correcto')
+    else
+        console.log('No se guardo correctamente los datos')
+}
+
+export const borrarNube= () => new Promise((resolve, reject) =>{  //codigo copiado desde la documentacion de firebase
+    DbFirestore.collection('boveda').get()
+    .then((snapshot) => {
+        // When there are no documents left, we are done
+        if (snapshot.size === 0) {
+         resolve(0);
+        }
+
+        // Delete documents in a batch
+        let batch = DbFirestore.batch();
+        snapshot.docs.forEach((doc) => {
+        batch.delete(doc.ref);
+        });
+
+        return batch.commit().then(() => {
+            resolve(snapshot.size);
+        })
+    }
+    )
+})
+
